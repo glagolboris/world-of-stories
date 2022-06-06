bbfc2c75b8ac10897203e73b08842ca1a7019656
Boris Glagolevskiy <92990171+glagolboris@users.noreply.github.com>
2022-06-06 23:36:13 +0300

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,67 @@
+def start_bttn():
+    return 'Начать ✅'
+
+def doc():
+    return 'Подтвердить 🔰'
+
+def start_anket():
+    return 'Заполнить анкету 📝'
+
+def start():
+    return 'Начать! 👀'
+
+def comedy():
+    return 'Юмор 🎭'
+
+def short():
+    return 'Короткий рассказ 📘'
+
+def nauch_pop():
+    return 'Научная фантастика 🧬'
+
+def adventure():
+    return 'Приключение 🍃'
+
+def detective():
+    return 'Детектив 🕵️‍'
+
+def roman():
+    return 'Роман 💖'
+
+def thriller():
+    return 'Триллер 🩸'
+
+def horror():
+    return 'Ужасы 🧟‍'
+
+def fantasy():
+    return 'Фэнтези 🎆'
+
+def poet():
+    return 'Поэзия 🪶'
+
+def battle():
+    return 'Боевик 💣'
+
+def other():
+    return 'Другое 👀'
+
+def all_genres():
+    return [
+        comedy(), short(), nauch_pop(), adventure(), roman(),
+        thriller(), horror(), fantasy(), poet(), battle(),
+        other()
+    ]
+
+def yes():
+    return 'Да! 🌈'
+
+def no():
+    return 'Нет 🌚'
+
+def check_true():
+    return 'Опубликовать ✅'
+
+def check_false():
+    return 'Не опубликовывать ❌'
+
