7f0309e1bb73d06fd50014439842a4133565b02c
Boris Glagolevskiy <92990171+glagolboris@users.noreply.github.com>
2022-06-06 23:35:31 +0300

    Create api.py

---
+++ api.py
@@ -0,0 +1,11 @@
+def token() -> str:
+    api = '5122367868:AAFSLeVmfxySo9lJrAsZ5bA2cAH3qE_G-rQ'
+    return api
+
+def group_token() -> str:
+    api = '-732503265'
+    return api
+
+def public_token() -> str:
+    api = '-1001636567650'
+    return api
